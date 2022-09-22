a3fa924a1a0b68303ae44bab5cb36f128bf9dd41
Árpás Péter <arpaspeter0@gmail.com>
2022-09-22 15:00:28 +0200

    első két gombig

--- app/src/main/java/com/example/a0922gyak/MainActivity.java
+++ app/src/main/java/com/example/a0922gyak/MainActivity.java
@@ -3,12 +3,61 @@ package com.example.a0922gyak;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText szovegbox;
+    private Button gomb1;
+    private Button gomb2;
+    private Button gomb3;
+    private TextView eredmeny;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
     }
+
+    private void gomb1click(){
+
+
+
+    }
+
+    private void init(){
+
+        szovegbox = findViewById(R.id.szovegbox);
+        gomb1 = findViewById(R.id.gomb1);
+        gomb2 = findViewById(R.id.gomb2);
+        gomb3 = findViewById(R.id.gomb3);
+        eredmeny = findViewById(R.id.eredmeny);
+
+        gomb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eredmeny.setText(szovegbox.getText().toString().toUpperCase());
+
+
+            }
+        });
+        gomb2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eredmeny.setText(szovegbox.getText().toString().toLowerCase());
+
+
+            }
+        });
+
+
+    }
+
 }
